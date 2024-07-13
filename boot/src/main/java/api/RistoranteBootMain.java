d28d993df38b4e7228eb0e14a144603dd1e0db9c
Nachorly <nachobjl.99@gmail.com>
2024-07-13 21:13:07 +0200

    Reservation Controller

--- boot/src/main/java/api/RistoranteBootMain.java
+++ boot/src/main/java/api/RistoranteBootMain.java
@@ -2,8 +2,10 @@ package api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"controllers"})
 public class RistoranteBootMain {
     public static void main(String[] args) {
         SpringApplication.run(RistoranteBootMain.class, args);
