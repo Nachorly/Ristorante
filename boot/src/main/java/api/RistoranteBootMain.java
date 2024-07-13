21776183324dd0fa17ecf977ae193521569060c7
Nachorly <nachobjl.99@gmail.com>
2024-07-13 19:32:05 +0200

    Ristorante API Initialization

---
+++ boot/src/main/java/api/RistoranteBootMain.java
@@ -0,0 +1,11 @@
+package api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RistoranteBootMain {
+    public static void main(String[] args) {
+        SpringApplication.run(RistoranteBootMain.class, args);
+    }
+}
