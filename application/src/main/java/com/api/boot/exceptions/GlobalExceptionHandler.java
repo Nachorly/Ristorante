857d8eb2ef2680cd9ad28d70dfb62989d50b5a2f
Nachorly <nachobjl.99@gmail.com>
2024-09-23 18:00:10 +0200

    Global Exception Handler

---
+++ application/src/main/java/com/api/boot/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.api.boot.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ infrastructure/src/main/java/com/api/infrastructure/db/ReservationRepositoryImpl.java
@@ -0,0 +1,7 @@
+package com.api.infrastructure.db;
+
+import com.api.domain.respository.ReservationRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class ReservationRepositoryImpl implements ReservationRepository {
+}
